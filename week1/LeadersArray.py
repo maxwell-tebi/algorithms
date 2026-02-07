b9af07944ded795ad8e2bfa638f529dc0dd50063
maxwell-tebi <maxwelltebi2@gmail.com>
2026-02-07 23:33:44 +0000

    Update LeadersArray.py

--- week1/LeadersArray.py
+++ week1/LeadersArray.py
@@ -1,11 +1,10 @@
-class Solution:
-    def leaders(self, arr):
-        leaders = []
-        max_from_right = float('-inf')
+def leaders(self, arr):
+    leaders = []
+    max_from_right = float('-inf')
         
         # Traverse from right to left
-        for i in range(len(arr) - 1, -1, -1):
-            if arr[i] >= max_from_right:
+    for i in range(len(arr) - 1, -1, -1):
+        if arr[i] >= max_from_right:
                 leaders.append(arr[i])
                 max_from_right = arr[i]
         
