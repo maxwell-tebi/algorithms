ab50b333e7f52675220c92995a4a933a1b174c1a
maxwell-tebi <maxwelltebi2@gmail.com>
2026-02-07 23:34:05 +0000

    Update LeadersArray.py

--- week1/LeadersArray.py
+++ week1/LeadersArray.py
@@ -5,9 +5,9 @@ def leaders(self, arr):
         # Traverse from right to left
     for i in range(len(arr) - 1, -1, -1):
         if arr[i] >= max_from_right:
-                leaders.append(arr[i])
-                max_from_right = arr[i]
+            leaders.append(arr[i])
+            max_from_right = arr[i]
         
-        leaders.reverse()
+    leaders.reverse()
         
         return leaders
