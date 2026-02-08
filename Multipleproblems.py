d85186d0ba39abb36280727577e8189e8b27b5b8
maxwell-tebi <maxwelltebi2@gmail.com>
2026-02-08 05:48:57 +0000

    Update Multipleproblems.py

--- Multipleproblems.py
+++ Multipleproblems.py
@@ -1,12 +1,11 @@
-# Multiple problems
+# Introduction to mathematical problem solving with codes
 
-class Solution:
-    def findSum(self, n):
+def findSum(self, n):
+    sum = 0
+    if n == 0:
         sum = 0
-        if n == 0:
-            sum = 0
-            return 0
-        else:
-            sum = n * (n+1) // 2
-            return sum
+        return 0
+    else:
+        sum = n * (n+1) // 2
+        return sum
         
