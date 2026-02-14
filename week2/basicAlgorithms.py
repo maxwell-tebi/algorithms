04b77c0346e7f18a78e2e575e2f18c1aeda33327
maxwell-tebi <maxwelltebi2@gmail.com>
2026-02-14 04:58:13 +0000

    Update basicAlgorithms.py

--- week2/basicAlgorithms.py
+++ week2/basicAlgorithms.py
@@ -21,7 +21,7 @@ class Solution:
 
 
 #Given two arrays a[] and b[], your task is to determine whether b[] is a subset of a[].
-
+from collections import Counter
 class Solution:
     #Function to check if a is a subset of b.
     def isSubset(self, a, b):
