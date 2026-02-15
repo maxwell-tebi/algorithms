acd4cf8b9d3035e98e09ad3e456f9b356e2055ff
maxwell-tebi <maxwelltebi2@gmail.com>
2026-02-15 07:37:35 +0000

    Update basicAlgorithms.py

--- week2/basicAlgorithms.py
+++ week2/basicAlgorithms.py
@@ -35,4 +35,4 @@ class Solution:
     
     
     
-#
+#Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
