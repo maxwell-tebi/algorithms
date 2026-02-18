cc5b1114fe9d0a29e30ea45ae48a2e4ac541001a
maxwell-tebi <maxwelltebi2@gmail.com>
2026-02-18 06:34:38 +0000

    Update main.py

--- week3/main.py
+++ week3/main.py
@@ -5,3 +5,11 @@ class Solution:
       
 
 #
+class Solution:
+   
+    def searchInSorted(self,arr, k):
+        
+        if k in arr:
+            return True
+        else:
+            return False
