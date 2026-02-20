4c3eea41511fe2804e9825f55b0b3563826baca4
maxwell-tebi <maxwelltebi2@gmail.com>
2026-02-20 07:01:41 +0000

    Update main.py

--- week3/main.py
+++ week3/main.py
@@ -14,7 +14,7 @@ class Solution:
         else:
             return False
 
-#You are given an integer n. You have  to print all numbers from 1 to n.Note: You must use recursion only, and print all numbers from 1 to n in a single line, separated by spaces.
+#You are given an integer n. You have  to print all numbers from 1 to n. Note: You must use recursion only, and print all numbers from 1 to n in a single line, separated by spaces.
 class Solution:    
     def printNos(self,n):
         if n>0:
