6b3c60f778fef153233e84aac3c871785b98d26f
maxwell-tebi <maxwelltebi2@gmail.com>
2026-02-22 20:25:01 +0000

    Update main.py

--- week3/main.py
+++ week3/main.py
@@ -33,6 +33,4 @@ class Solution:
                 
         
         return sequence
-#Given an array arr[]. Your task is to find the elements whose value is equal to that of its index value ( Consider 1-based indexing ).
-
-Note: There can be more than one element in the array which have the same value as its index. You need to include every such element's index. Follows 1-based indexing of the array.
+#Given an array arr[]. Your task is to find the elements whose value is equal to that of its index value ( Consider 1-based indexing ). Note: There can be more than one element in the array which have the same value as its index. You need to include every such element's index. Follows 1-based indexing of the array.
