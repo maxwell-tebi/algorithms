baa4502e32df07c00d9e56818733943c1f3f093d
maxwell-tebi <maxwelltebi2@gmail.com>
2026-02-23 22:47:50 +0000

    Update main.py

--- week4/main.py
+++ week4/main.py
@@ -1,1 +1,14 @@
 #Given two integers a and b, You have to compute their LCM and GCD and return an array containing their LCM and GCD.
+class Solution:
+    def lcmAndGcd(self, a: int, b: int) -> List[int]:
+        # Euclidean algorithm for GCD
+        Big = max(a, b)
+        Small = min(a, b)
+        
+        while Small != 0:
+            Big, Small = Small, Big % Small
+        
+        GCD = Big  # GCD is the last non-zero value
+        LCM = abs(a * b) // GCD  # Use integer division
+        
+        return [LCM, GCD]
