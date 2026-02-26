9a0690cd6ccd37c0259ca55dfc98dcefb3ec5d49
maxwell-tebi <maxwelltebi2@gmail.com>
2026-02-26 19:11:16 +0000

    Update main.py

--- week4/main.py
+++ week4/main.py
@@ -26,7 +26,7 @@ def factorial(self, n: int) -> int:
         
     fact = 1
         
-        for i in range(1, n+1):
+    for i in range(1, n+1):
             fact *= i
             
         return fact
